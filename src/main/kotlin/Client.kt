d3e5e85f6b4e263d7608679e2a6084c9d76c1530
Kamil Kiewisz <kamkie@outlook.com>
2018-11-01 16:49:58 +0100

    request channel

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,11 +1,17 @@
+package com.example.demo.rsocket
+
 import io.rsocket.AbstractRSocket
 import io.rsocket.Payload
+import io.rsocket.RSocket
 import io.rsocket.RSocketFactory
 import io.rsocket.transport.netty.client.TcpClientTransport
 import io.rsocket.transport.netty.server.TcpServerTransport
 import io.rsocket.util.DefaultPayload
 import mu.KotlinLogging
+import org.reactivestreams.Publisher
+import reactor.core.publisher.EmitterProcessor
 import reactor.core.publisher.Flux
+import reactor.core.publisher.FluxSink
 import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
 import java.time.Duration
@@ -14,19 +20,24 @@ import java.util.concurrent.atomic.AtomicLong
 
 val logger = KotlinLogging.logger {}
 
-val sch3 = Schedulers.newParallel("baz", 6)
+val clientScheduler = Schedulers.newParallel("clientScheduler", 1)
+val serverScheduler = Schedulers.newParallel("serverScheduler", 2)
+val serverScheduler2 = Schedulers.newParallel("serverScheduler", 2)
 
 fun main() {
     RSocketFactory.receive()
             .acceptor { setup, rs ->
                 Mono.fromCallable {
                     object : AbstractRSocket() {
-                        override fun requestStream(payload: Payload): Flux<Payload> {
-                            logger.info { "new request ${payload.dataUtf8}" }
-                            return Flux.range(0, 10_000_000)
-//                                    .publishOn(sch3)
-//                                    .subscribeOn(sch3)
+                        override fun requestChannel(payload: Publisher<Payload>): Flux<Payload> {
+                            return Flux.from(payload)
+                                    .map { it.dataUtf8 }
+                                    .doOnNext { logger.info { "incoming message $it" } }
+                                    .flatMap { Flux.range(0, 10_000_000) }
                                     .map { DefaultPayload.create("Interval: $it") }
+//                                    .publishOn(serverScheduler2)
+                                    .subscribeOn(serverScheduler)
+//                                    .onBackpressureDrop()
                         }
                     }
                 }
@@ -42,34 +53,37 @@ fun main() {
 
     val count = AtomicLong(0)
 
-    logger.info { "first" }
-    socket
-            .requestStream(DefaultPayload.create("Hello 1"))
-            .map { it.dataUtf8 }
-            .doOnNext { count.incrementAndGet() }
-            .subscribeOn(sch3)
-            .subscribe()
-
-    logger.info { "second" }
-    socket
-            .requestStream(DefaultPayload.create("Hello 2"))
-            .map { it.dataUtf8 }
-            .doOnNext { count.incrementAndGet() }
-            .subscribeOn(sch3)
-            .subscribe()
-
-    logger.info { "third" }
-    socket
-            .requestStream(DefaultPayload.create("Hello 3"))
-            .map { it.dataUtf8 }
-            .doOnNext { count.incrementAndGet() }
-            .subscribeOn(sch3)
-            .subscribe()
-
     Flux.interval(Duration.ofSeconds(1))
             .doOnNext {
                 val countInOneSecond = count.getAndSet(0)
                 logger.info { "count in 1s: $countInOneSecond" }
             }
-            .then().block()
+            .then().subscribe()
+
+    val sink = sendRequest(socket, count, "first")
+
+    sink.next(DefaultPayload.create("first"))
+    sink.next(DefaultPayload.create("second"))
+    sink.next(DefaultPayload.create("third"))
+
+    Thread.currentThread().join()
+}
+
+private fun sendRequest(socket: RSocket, count: AtomicLong, rid: String): FluxSink<Payload> {
+    logger.info { rid }
+    val emitterProcessor = EmitterProcessor.create<Payload>(100_000)
+    val sink = emitterProcessor.sink(FluxSink.OverflowStrategy.LATEST)
+
+    socket
+            .requestChannel(emitterProcessor)
+            .map { it.dataUtf8 }
+            .doOnSubscribe { logger.info { "$rid started" } }
+            .doOnNext { count.incrementAndGet() }
+            .doOnError { logger.info { "$rid error" } }
+            .doOnComplete { logger.info { "$rid completed" } }
+            .doOnCancel { logger.info { "$rid canceled" } }
+            .subscribeOn(clientScheduler)
+            .subscribe()
+
+    return sink
 }
